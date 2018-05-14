3f886f28ad6803a8d400243dc0c585d96d6e0b58
MANISH <manishrncg@gmail.com>
2018-05-14 22:00:12 +0530

    product_lsiting name changed to item

---
+++ app/components/Item.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Item extends React.Component {
+  render() {  // Basic component for every product being displayed while listing
+    const {name, image, price} = this.props;
+  
+    return (<div className="col-md-3 col-sm-3 col-xs-3 inline productBox" >
+          <img src={image} alt={name} className="productListImage" />
+          <p>{name}</p>
+          <b>&#8377; {price.toFixed(2)} </b>
+        </div>);
+  }
+}
+
+export default Item;
